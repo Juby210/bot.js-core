0ac84b24fdd01406f2850b64be3410646109100c
juby210-PL <juby210@vp.pl>
2019-06-19 23:48:33 +0200

    fix cmd usage embed

--- core_plugins/embeds.js
+++ core_plugins/embeds.js
@@ -31,6 +31,6 @@ module.exports.erreb = class BJSErrorEmbed extends BJSEmbed {
 
 module.exports.cmdusage = class extends BJSEmbed {
     constructor(cmd, prefix) {
-        super('warn', { description: `Usage:\n\n\`${prefix}${cmd.triggers[0]} ${cmd.usage.join(`\`\n\`${prefix}${cmd.triggers[0]} `)}` })
+        super('warn', { description: `Usage:\n\n\`${prefix}${cmd.triggers[0]} ${cmd.usage.join(`\`\n\`${prefix}${cmd.triggers[0]} `)}\`` })
     }
 }
