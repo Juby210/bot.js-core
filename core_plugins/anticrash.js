4c56c772b53fecd8922e2012fbe3c8b394a42257
juby210-PL <juby210@vp.pl>
2019-06-19 19:56:40 +0200

    typo

--- core_plugins/anticrash.js
+++ core_plugins/anticrash.js
@@ -4,7 +4,7 @@ module.exports = (message, err, send = true) => {
     const code = Math.floor(1000 + Math.random() * 9000)
 
     const eb = new Core.embeds.erreb()
-    eb.setDescription(`This shouldn't have happend.\nSend this code to developer: **#${code}**\n\n[Support server](https://discord.gg/6bfpCCt)\n\n\`${err}\``)
+    eb.setDescription(`This shouldn't have happened.\nSend this code to developer: **#${code}**\n\n[Support server](https://discord.gg/6bfpCCt)\n\n\`${err}\``)
     if(message) {
         message.channel.send(eb)
         if(send) eb.addField('Command', message.content)
